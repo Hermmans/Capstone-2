ae98c3e85ea7dba0c8c75382434e1f4123d4c82b
Bradley <bradleydavid603@gmail.com>
2022-06-11 22:43:09 -0400

    current balance/view transfer history/send bucks work

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.techelevator.tenmo.controller;
+
+import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@PreAuthorize("isAuthenticated()")
+public class UserController {
+
+    private UserDao userDao;
+
+    @Autowired
+    public UserController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    //END POINTS that have methods associated with them
+
+    @PreAuthorize("permitAll")
+    @RequestMapping(path = "user/{id}", method = RequestMethod.GET)
+    public User listUserById(@PathVariable Long id) {
+        return userDao.findById(id);
+    }
+
+    @PreAuthorize("permitAll")
+    @RequestMapping(path = "user/", method = RequestMethod.GET)
+    public User[] listAllUsers() {
+        return userDao.findAll();
+    }
+
+    ////////////////////////////////////////////////
+}
